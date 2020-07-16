efd6a667a8b6129471aba2b4847c1836965df78c
k1921 <k1921@student.jamk.fi>
2020-07-16 14:25:47 +0300

    working

--- src/App.js
+++ src/App.js
@@ -3,199 +3,32 @@ import "./App.css";
 import Board from "./components/Board";
 
 function App() {
-  const boardSize = 10;
   let initialNumbers = [1, 2, 3];
-  const [clicks, setClicks] = useState([]);
-
-  useEffect(() => {
-    console.log("----->APP<-----");
-    setNumbers(initialNumbers);
-    setNumbersCopy(initialNumbers);
-    setNumberCount(initialNumbers.length); //setupNextRound(initialNumbers);
-    //setNumberCount(initialNumbers.length);
-  }, []);
-
-  const [nextRoundNumbers, setNextRoundNumbers] = useState();
   const [numbers, setNumbers] = useState();
-  const [numbersCopy, setNumbersCopy] = useState(numbers);
-  const [numberCount, setNumberCount] = useState(numbers);
-  const [playBoard, setPlayBoard] = useState();
 
   useEffect(() => {
-    //setupNextRound(numbers);
-    //setNumberCount(numbers.length);
+    console.log("----->APP USEEFFECT<-----");
+    setNumbers(initialNumbers);
   }, []);
 
-  const setupNextRound = async (nums) => {
-    console.log("setupNextRound", nums);
-    //setNumbers(nums);
+  function nextRound(numbers) {
+    let nums = [...numbers, numbers.pop() + 1];
     setNumbers();
     setNumbers(nums);
-
-    console.log("setupNextRound numbers", numbers);
-  };
-
-  function randomRectangleIndexes(numbersLen) {
-    let randomIndexes = [];
-    while (true) {
-      let num = getRandomInt(10).toString() + getRandomInt(10).toString();
-      if (!randomIndexes.includes(num)) {
-        randomIndexes.push(num);
-      }
-      if (randomIndexes.length === numbersLen) break;
-    }
-    return randomIndexes;
   }
-
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  const addClassName = () => {
-    let els = document.querySelectorAll(".rectangle-playable");
-    els.forEach(function (el) {
-      el.classList.add("rectangle-hide-text");
-      //el.className = "rectangle-hide-text";
-    });
-  };
-
-  const popRandom = (array) => {
-    let random = array.splice(Math.floor(Math.random() * array.length), 1);
-    return random;
-  };
-
-  const updateClickOrder = (order, el) => {
-    order.push(el);
-  };
-  let clickOrder = [];
-  const fillBoard = (indexes) => {
-    let board = [];
-
-    for (let i = 0; i < boardSize; i++) {
-      for (let j = 0; j < boardSize; j++) {
-        if (indexes.includes(i.toString() + j.toString())) {
-          board.push(
-            <div
-              key={i.toString() + j.toString()}
-              className="rectangle-playable"
-              id={i.toString() + j.toString()}
-              onClick={(clickCount) => {
-                //setClickCount(clickCount + 1); //this doesnt work
-
-                var el = document.getElementById(i.toString() + j.toString());
-
-                //clickOrder.push(el.innerHTML);
-
-                updateClickOrder(clickOrder, el.innerHTML);
-                /* console.log("numberCount", numberCount);
-                console.log("clickOrder", clickOrder.length);
-                console.log("clickOrder", clickOrder); */
-
-                el.className = "rectangle-clicked";
-                addClassName();
-
-                if (clickOrder.length === numberCount) {
-                  setClicks(clickOrder);
-                  setPlayBoard();
-                }
-              }}
-            >
-              {popRandom(numbersCopy)}
-              {/* {numbers.pop(0)} */}
-            </div>
-          );
-        } else {
-          board.push(
-            <div
-              key={i.toString() + j.toString()}
-              className="rectangle"
-              id={i.toString() + j.toString()}
-            >
-              &nbsp;
-            </div>
-          );
-        }
-      }
-      board.push(<br />);
-    }
-    //console.log("coardb", board);
-    return board;
-  };
-  const checkRoundPassed = (clicks) => {
-    console.log("clicks", clicks);
-    let nums = [];
-    let i = 0;
-    let result = clicks.map((c) => {
-      nums[i] = ++i;
-
-      return parseInt(c) === i;
-    });
-    console.log("resulst", result);
-    return result.includes(false) ? (
-      <h2 style={{ color: "DarkOrange" }}> Round failed </h2>
-    ) : (
-      <>
-        <h2 style={{ color: "PaleGreen" }}> Round passed ! </h2>
-        <p>
-          Your click order{" "}
-          {clicks.map((c) => (
-            <>&nbsp;{c}</>
-          ))}
-        </p>
-        <button
-          onClick={() => {
-            console.log("CHECKROUNDPASSED numbers", nums);
-
-            setupNextRound(nums.concat(nums[nums.length - 1] + 1));
-          }}
-        >
-          Next Round
-        </button>
-      </>
-    );
-  };
-  const setBoard = (numbers) => {
-    console.log("setBoard", numbers);
-
-    return (
-      <Board
-        numbers={numbers}
-        setupNextRound={setupNextRound}
-        checkRoundPassed={checkRoundPassed}
-        randomRectangleIndexes={randomRectangleIndexes}
-        fillBoard={fillBoard}
-        clicks={clicks}
-      />
-    );
-  };
 
   return numbers ? (
     <>
-      {console.log("APPPPPPP")}
-      <button
-        onClick={() => {
-          console.log("numbers", numbers);
-          let nubs = [...numbers];
-          console.log("nubs", nubs);
-          setNumbers();
-          setNumbers(nubs);
-          setBoard(numbers);
-        }}
-      >
-        BUTTON
-      </button>
       <div className="app">
         <header className="header">
-          <h1>Chimp test</h1>
+          <h1>The Chimp Test</h1>
+          <p>can you outsmart a chimp</p>
         </header>
-        {/* {console.log("numbers", numbers)} */}
-        {setBoard(numbers)}
+        {<Board numbers={numbers} nextRound={nextRound} />}
       </div>
     </>
   ) : (
-    <>
-      {/* {console.log("numbers", numbers)}
-      {console.log("numbersCopy", numbersCopy)} */}
-    </>
+    <></>
   );
 }
 
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,40 +1,125 @@
 import React, { useState, useEffect } from "react";
 
-const Board = ({
-  numbers,
-  setupNextRound,
-  checkRoundPassed,
-  randomRectangleIndexes,
-  fillBoard,
-  clicks,
-}) => {
+const boardSize = 7;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+const popRandom = (array) => {
+  let random = array.splice(Math.floor(Math.random() * array.length), 1);
+  return random;
+};
+
+function randomRectangleIndexes(numbersLen) {
+  let randomIndexes = [];
+  while (true) {
+    let num =
+      getRandomInt(boardSize).toString() + getRandomInt(boardSize).toString();
+    if (!randomIndexes.includes(num)) {
+      randomIndexes.push(num);
+    }
+    if (randomIndexes.length === numbersLen) break;
+  }
+  return randomIndexes;
+}
+
+const Board = ({ numbers, nextRound }) => {
   const [board, setBoard] = useState();
-  const [numLen, setNumLen] = useState(numbers.length);
-  const [nums, setNums] = useState(numbers);
+  const [roundPass, setRoundPass] = useState();
+
+  const hideRectangleTexts = () => {
+    let els = document.querySelectorAll(".rectangle-playable");
+    els.forEach(function (el) {
+      el.classList.add("rectangle-hide-text");
+    });
+  };
+  const fillBoard = (indexes, numbers) => {
+    let numbersCopy = [...numbers];
+    let board = [];
+    var clickCount = 0;
+
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        if (indexes.includes(i.toString() + j.toString())) {
+          board.push(
+            <div
+              key={i.toString() + j.toString()}
+              className="rectangle-playable"
+              id={i.toString() + j.toString()}
+              onClick={() => {
+                hideRectangleTexts();
+                var el = document.getElementById(i.toString() + j.toString());
+                el.className = "rectangle-clicked";
+                let elInnerHTML = el.innerHTML;
+
+                if (!(numbers[clickCount] === parseInt(elInnerHTML))) {
+                  setRoundPass(false);
+                  clickCount = 0;
+                } else if (clickCount === numbers.length - 1) {
+                  setRoundPass(true);
+                  clickCount = 0;
+                } else {
+                  clickCount++;
+                }
+              }}
+            >
+              {popRandom(numbersCopy)}
+              {/* {numbers.pop(0)} */}
+            </div>
+          );
+        } else {
+          board.push(
+            <div
+              key={i.toString() + j.toString()}
+              className="rectangle"
+              id={i.toString() + j.toString()}
+            >
+              &nbsp;
+            </div>
+          );
+        }
+      }
+      board.push(<br />);
+    }
+    return board;
+  };
 
   useEffect(() => {
-    console.log("EFFECT numbers", numbers);
-    setNums(numbers);
-    console.log("board numbers.length", numbers.length);
-    var randomIndexes = randomRectangleIndexes(numbers.length);
-    const b = fillBoard(randomIndexes);
-
+    let b = fillBoard(randomRectangleIndexes(numbers.length), numbers);
     setBoard(b);
-    //console.log("b", b);
-  }, []);
+  }, [numbers]);
 
-  return board ? (
-    (console.log("board numbers", numbers),
-    console.log("board nums", nums),
-    document.getElementsByClassName("rectangle-clicked").length < numLen ? (
-      <>
-        <div className="board">{board}</div>
-      </>
-    ) : (
-      <div className="board">{checkRoundPassed(clicks)}</div>
-    ))
+  return roundPass === undefined ? (
+    <div className="board">{board}</div>
+  ) : roundPass === true ? (
+    <div className="win-fail">
+      <h2>Good job!</h2>
+      <br />
+      <button
+        onClick={() => {
+          setRoundPass();
+          nextRound(numbers);
+        }}
+      >
+        Next round
+      </button>
+    </div>
   ) : (
-    <>WHTA</>
+    <div className="win-fail">
+      <h2>Round failed!</h2>
+      <br />
+      <button
+        onClick={() => {
+          setRoundPass();
+          /* draws the same board */
+        }}
+      >
+        Retry
+      </button>
+      {/*  TODO new button draw new board 
+            setBoard(fillboard....)
+          */}
+    </div>
   );
 };
 export default Board;
