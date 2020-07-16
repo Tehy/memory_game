6b817ba4f677a501b2a64f676532b83b9cd3bb1c
k1921 <k1921@student.jamk.fi>
2020-07-16 15:54:14 +0300

    Updates

---
+++ precache-manifest.eb06f1a5830307a003c1cd429abf0f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7f25a2cb683a82d03685b5995d8ab7",
+    "url": "/memory_game/index.html"
+  },
+  {
+    "revision": "3b36b0cf5306f733de64",
+    "url": "/memory_game/static/css/main.20055f1b.chunk.css"
+  },
+  {
+    "revision": "8011a8ee3ca366f8c8f8",
+    "url": "/memory_game/static/js/2.61fa9a80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory_game/static/js/2.61fa9a80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b36b0cf5306f733de64",
+    "url": "/memory_game/static/js/main.fbab876a.chunk.js"
+  },
+  {
+    "revision": "b0dc2204c441cbf8d9af",
+    "url": "/memory_game/static/js/runtime-main.b96f13f6.js"
+  }
+]);
